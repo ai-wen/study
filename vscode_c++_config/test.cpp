37584ab0eb0e3d791d58115547084a725d5fc66c
ai-wen <363042868@qq.com>
2020-02-14 18:50:51 +0800

    初次提交

---
+++ vscode_c++_config/test.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1;
+    int b = 2;
+    int c = 0;
+
+    c = a + b;
+    
+    printf("%d + %d = %d", a, b, c);
+
+   // getchar();
+    
+    return 0;
+}
+
